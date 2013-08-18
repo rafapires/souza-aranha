1e26288835bb9aeb32c1d97578b117281be908e4
Rafael Pires <rafael.pires.sp@gmail.com>
2013-08-18 02:25:48 -0300

    html da home colocada diretamente na pagina do wordpress

---
+++ wp-content/themes/tema2013/home.php
@@ -0,0 +1,123 @@
+<div id="myCarousel" class="carousel slide">
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="http://dummyimage.com/1020x300/eee/fff.png" alt="">
+            <div class="carousel-caption">
+                <h4>First Thumbnail label</h4>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="http://dummyimage.com/1020x300/eee/fff.png" alt="">
+            <div class="carousel-caption">
+                <h4>Second Thumbnail label</h4>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="http://dummyimage.com/1020x300/eee/fff.png" alt="">
+            <div class="carousel-caption">
+                <h4>Third Thumbnail label</h4>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+        </div>
+    </div>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+</div>
+<div id="sa_call_actions" class="row">
+    <div class="span4">
+        <h1 class="sa_title_call_action">Ultimos Blogs</h1>
+        <ul>
+            <li>
+                <h1>O FATOR ZERO DOS PROGRAMAS DE FIDELIDADE</h1>
+                <p>Você considera defensável o desenvolvimento de uma campanha de propaganda sem estar baseada em uma estratégia de marketing?<br/>O fato de estar fundam...</p>
+                <a href="#" class="btn btn-small pull-right">leia mais</a>
+                <hr>
+            </li>
+            <li>
+                <h1>O FATOR ZERO DOS PROGRAMAS DE FIDELIDADE</h1>
+                <p>Você considera defensável o desenvolvimento de uma campanha de propaganda sem estar baseada em uma estratégia de marketing?<br/>O fato de estar fundam...</p>
+                <a href="#" class="btn btn-small pull-right">leia mais</a>
+                <hr>
+            </li>
+            <li>
+                <h1>O FATOR ZERO DOS PROGRAMAS DE FIDELIDADE</h1>
+                <p>Você considera defensável o desenvolvimento de uma campanha de propaganda sem estar baseada em uma estratégia de marketing?<br/>O fato de estar fundam...</p>
+                <a href="#" class="btn btn-small pull-right">leia mais</a>
+                <hr>
+            </li>
+            <li>
+                <h1>O FATOR ZERO DOS PROGRAMAS DE FIDELIDADE</h1>
+                <p>Você considera defensável o desenvolvimento de uma campanha de propaganda sem estar baseada em uma estratégia de marketing?<br/>O fato de estar fundam...</p>
+                <a href="#" class="btn btn-small pull-right">leia mais</a>
+                <hr>
+            </li>
+        </ul>
+    </div>
+    <div id="sa_middle_action" class="span4">
+        <h1 class="sa_title_call_action">Próximo Webinar</h1>
+        <ul>
+            <li>
+                <h1>A Gestão Eficaz do Funil de Vendas</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie turpis in dui volutpat, ut iaculis est interdum</p>
+                <div class="well well-small well-warning">
+                    <h2>Inscrições Gratuítas</h2>
+                    <p>Faça sua pré-inscrição no Fale Conosco e aproveite e faça o download de <a href="#">As Melhores Práticas na Gestão do Funil de Vendas – Sales Pipeline Management.</a></p>
+                    <a href="#" class="btn btn-primary btn-block">Reserve já sua vaga</a>
+                </div>
+            </li>
+        </ul>
+        <h1 class="sa_title_call_action">Webinars anteriores</h1>
+        <ul>
+            <li>
+                <h1>A Gestão Eficaz do Funil de Vendas</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie turpis in dui volutpat, ut iaculis est interdum</p>
+                <a href="#" class="btn btn-small pull-right">Assista</a>
+                <hr>
+            </li>
+            <li>
+                <h1>A Gestão Eficaz do Funil de Vendas</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie turpis in dui volutpat, ut iaculis est interdum</p>
+                <a href="#" class="btn btn-small pull-right">Assista</a>
+                <hr>
+            </li>
+            <li>
+                <h1>A Gestão Eficaz do Funil de Vendas</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie turpis in dui volutpat, ut iaculis est interdum</p>
+                <a href="#" class="btn btn-small pull-right">Assista</a>
+                <hr>
+            </li>
+        </ul>
+    </div>
+    <div class="span4">
+        <h1 class="sa_title_call_action">Nosso diferencial</h1>
+        <ul>
+            <li>
+                <h1>Mercado de Consumo</h1>
+                <p>Desenvolvemos cases de sucesso em relacionamento para empresas nas mais diversas áreas do mercado de consumo</p>
+                <hr>
+            </li>
+            <li>
+                <h1>Mercado B to B</h1>
+                <p>Programas de alto desempenho para aumentar a competitividade e produtividade da sua força de vendas.</p>
+                <hr>
+            </li>
+            <li>
+                <h1>Agências</h1>
+                <p>Por ser uma consultoria indenpendente, a Souza Aranha é uma aliada das agências no desenvolvimento de soluções de relacionamento.</p>
+                <hr>
+            </li>
+            <li>
+                <h1>Informação</h1>
+                <p>Para a área de informação de Marketing, a Souza Aranha auxilia a conhecer seus clientes e facilitar a atualização e integração de dados.</p>
+                <hr>
+            </li>
+        </ul>
+    </div>
+</div>
