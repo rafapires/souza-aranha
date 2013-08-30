5b9607662fff20a3475739c105223f8cafe1e7ce
Marcelo Souza <marcelo@basemix.com.br>
2013-08-30 17:16:16 -0300

    Cria codigo html de blogs

---
+++ wp-content/themes/tema2013/blogs.php
@@ -0,0 +1,79 @@
+<div id="myCarousel" class="carousel slide">
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="http://dummyimage.com/1020x300/eee/fff.png" alt="">
+            <div class="carousel-caption">
+                <h4>First Thumbnail label</h4>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="http://dummyimage.com/1020x300/eee/fff.png" alt="">
+            <div class="carousel-caption">
+                <h4>Second Thumbnail label</h4>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="http://dummyimage.com/1020x300/eee/fff.png" alt="">
+            <div class="carousel-caption">
+                <h4>Third Thumbnail label</h4>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+        </div>
+    </div>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+</div>
+
+<div id="sa_blogs">
+	<div class="row-fluid">
+		<ul class="thumbnails">
+			<li class="span3">
+				<div class="thumbnail">
+					<img src="http://dummyimage.com/100x100/ccc/0011ff">
+					<div class="caption">
+						<h2>Gestão Estratégica de Clientes</h2>
+						<p>Aumentar a rentabilidade da empresa por meio do aumento contínuo do valor de sua base de clientes, possibilitando resultados duradouros no relacionamento com os clientes de sua empresa.</p>
+					</div>
+					<a href="#" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
+				</div>
+			</li>
+			<li class="span3">
+				<div class="thumbnail">
+					<img src="http://dummyimage.com/300x200/eee/fff.png">
+					<div class="caption">
+						<h2>Programa de Fidelidade</h2>
+						<p>Motivar continuamente os clientes a concentrar as compras em sua marca, reforçar a retenção de clientes de valor e aumentar a competitividade da marca.</p>
+					</div>
+					<a href="<?php get_site_url(); ?>/?page_id=67" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
+				</div>
+			</li>
+			<li class="span3">
+				<div class="thumbnail">
+					<img src="http://dummyimage.com/300x200/eee/fff.png">
+					<div class="caption">
+						<h2>Lead Management</h2>
+						<p>Gerar, cultivar e qualificar os leads, auxiliando a força de vendas a identificar prospects com real propensão à compra de produtos e serviços de sua empresa, além de monitorar cada lead e cada oportunidade ao longo do funil de vendas.</p>
+					</div>
+					<a href="<?php echo site_url(); ?>/?page_id=67" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
+				</div>
+			</li>
+			<li class="span3">
+				<div class="thumbnail">
+					<img src="http://dummyimage.com/300x200/eee/fff.png">
+					<div class="caption">
+						<h2>Plataforma de CRM</h2>
+						<p>Permite a visão 360° do cliente, por meio da integração das informações das áreas de Vendas, Marketing e Serviços a Clientes em uma única base de dados. A Plataforma de CRM permite o aperfeiçoamento e a informatização dos processos de vendas e de serviços a clientes. Fortalece a infraestrutura de relacionamento com os clientes a menores custos.</p>
+					</div>
+					<a href="<?php echo site_url(); ?>/?page_id=67" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
