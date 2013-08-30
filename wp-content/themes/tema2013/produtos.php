808d0c7e07cb04286c2f129c273f4b5026026b1f
Rafael Pires <rafael.pires.sp@gmail.com>
2013-08-30 06:25:33 -0300

    coloca link para produto

--- wp-content/themes/tema2013/produtos.php
+++ wp-content/themes/tema2013/produtos.php
@@ -18,7 +18,7 @@
 						<h2>Programa de Fidelidade</h2>
 						<p>Motivar continuamente os clientes a concentrar as compras em sua marca, reforçar a retenção de clientes de valor e aumentar a competitividade da marca.</p>
 					</div>
-					<a href="#" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
+					<a href="<?php get_site_url(); ?>/?page_id=67" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
 				</div>
 			</li>
 			<li class="span3">
@@ -28,7 +28,7 @@
 						<h2>Lead Management</h2>
 						<p>Gerar, cultivar e qualificar os leads, auxiliando a força de vendas a identificar prospects com real propensão à compra de produtos e serviços de sua empresa, além de monitorar cada lead e cada oportunidade ao longo do funil de vendas.</p>
 					</div>
-					<a href="#" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
+					<a href="<?php echo site_url(); ?>/?page_id=67" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
 				</div>
 			</li>
 			<li class="span3">
@@ -38,7 +38,7 @@
 						<h2>Plataforma de CRM</h2>
 						<p>Permite a visão 360° do cliente, por meio da integração das informações das áreas de Vendas, Marketing e Serviços a Clientes em uma única base de dados. A Plataforma de CRM permite o aperfeiçoamento e a informatização dos processos de vendas e de serviços a clientes. Fortalece a infraestrutura de relacionamento com os clientes a menores custos.</p>
 					</div>
-					<a href="#" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
+					<a href="<?php echo site_url(); ?>/?page_id=67" class="btn btn-large btn-block btn-primary">Mais detalhes</a>
 				</div>
 			</li>
 		</ul>
