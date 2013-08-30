c1c174b37f8a157db8ea47b74f7559d8a7dfb5c5
Rafael Pires <rafael.pires.sp@gmail.com>
2013-08-30 06:24:06 -0300

    acerta title

--- wp-content/themes/tema2013/header.php
+++ wp-content/themes/tema2013/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>ADFAP - Adm. de bens e condominios</title>
+    <title>Souza Aranha</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Le styles -->
