4aea29866bd7a295b32729a19d76722448501766
celostad <celostad@gmail.com>
2013-10-01 11:35:10 -0300

    Altera a página webinars.php para a nova solicitação: último em destaque  e anteriores sem formatação do css

--- wp-content/themes/tema2013/webinars.php
+++ wp-content/themes/tema2013/webinars.php
@@ -4,22 +4,44 @@
  
 get_header(); ?>
 
-<div id="sa_produtos">
+<div>
+
+			<?php
+		$aRecentPosts = new WP_Query("post_type=webinars&post_por_page=1&showposts=1&order=desc&orderby=date");
+		while($aRecentPosts->have_posts()) : $aRecentPosts->the_post();
+		
+		$id_ultimoWebinar =  $post->ID;
+		?>
+			           <li>
+		                <h1><?php the_title(); ?></h1>
+		                	<?php the_content();?>
+		                <!-- <a href="<?php the_permalink(); ?>" class="btn btn-small pull-right">leia mais</a> -->
+		                <hr>
+		            </li>
+			<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+
+
 	<div class="row">
-		<ul class="thumbnails">
+		<ul>
  			<?php
- 			$newsArgs = array( 'post_type' => 'webinars', 'posts_per_page' => 4);                   
+ 			
+ 			$newsArgs = array(
+ 					'post_type'=> 'webinars',
+ 					'post_per_page'=>5,
+ 					'showposts'=>-1,
+ 					'post__not_in' => array($id_ultimoWebinar) ,
+ 					'order'    => 'DESC',
+ 			);
+ 			
 			$newsLoop = new WP_Query( $newsArgs );
 			while ( $newsLoop->have_posts() ) : $newsLoop->the_post();?>
 				<li class="span3">
-					<div class="thumbnail">
-						<?php the_post_thumbnail(); ?>
 						<div class="caption">
 							<h2><?php the_title(); ?></h2>
-							<?php echo the_excerpt(); ?>
+							<?php echo the_content(); ?>
 						</div>
-						<a href="<?php the_permalink(); ?>" class="btn btn-large btn-block btn-primary">Mais detalhes</a>				
-                    </div>
+						<a href="<?php the_permalink(); ?>">Mais detalhes</a>				
 				</li>
 			<?php endwhile; // end of the loop. ?>
 		</ul>
