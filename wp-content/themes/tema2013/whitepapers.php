a6e3548e1056b567c7db941d5bdd67ee91e9c519
celostad <celostad@gmail.com>
2013-10-01 10:40:41 -0300

    Cria a página whitepapers.php (chama através do menu)

---
+++ wp-content/themes/tema2013/whitepapers.php
@@ -0,0 +1,30 @@
+<?php 
+
+/*Template Name: Whitepapers */
+ 
+get_header(); ?>
+
+<div id="sa_produtos">
+	<div class="row">
+		<ul class="thumbnails">
+ 			<?php
+ 			$newsArgs = array( 'post_type' => 'whitepaper', 'posts_per_page' => 4);                   
+			$newsLoop = new WP_Query( $newsArgs );
+			while ( $newsLoop->have_posts() ) : $newsLoop->the_post();?>
+				<li class="span3">
+					<div class="thumbnail">
+						<?php the_post_thumbnail(); ?>
+						<div class="caption">
+							<h2><?php the_title(); ?></h2>
+							<?php echo the_excerpt(); ?>
+						</div>
+						<a href="<?php the_permalink(); ?>" class="btn btn-large btn-block btn-primary">Mais detalhes</a>				
+                    </div>
+				</li>
+			<?php endwhile; // end of the loop. ?>
+		</ul>
+	</div>
+</div>
+
+<?php wp_reset_query(); ?>
+<?php get_footer(); ?>
