c99467b514e580a59c5e549cbd9b70e482bf0127
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2014-11-04 06:12:35 -0200

    começa metodologias acerta container coloca margem topo pra caber header lista metodologias em 4 colunas

---
+++ wp-content/themes/sa2015/metodologias.php
@@ -0,0 +1,30 @@
+<?php 
+
+/*Template Name: Metodologias */
+ 
+get_header(); ?>
+
+<div id="sa_metodologias" class="container">
+	<div class="row">
+		<ul class="thumbnails">
+ 			<?php
+ 			$newsArgs = array( 'post_type' => 'metodologias', 'posts_per_page' => 4);                   
+			$newsLoop = new WP_Query( $newsArgs );
+			while ( $newsLoop->have_posts() ) : $newsLoop->the_post();?>
+				<li class="col-sm-3">
+					<div class="thumbnail">
+						<?php the_post_thumbnail('full'); ?>
+						<div class="caption">
+							<h2 class="sa_title_links"><?php the_title(); ?></h2>
+							<p><?php echo get_post_meta(get_the_ID(),'sa_diferencial_metodologia',true ); ?></p>
+						</div>
+						<a href="<?php the_permalink(); ?>" class="btn btn-large btn-block btn-primary">Mais detalhes</a>				
+                    </div>
+				</li>
+			<?php endwhile; // end of the loop. ?>
+		</ul>
+	</div>
+</div>
+
+<?php wp_reset_query(); ?>
+<?php get_footer(); ?>
